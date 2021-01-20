c195221ca38a0bff9732b05cd2e6e91ba029621d
ABDUL AZEEM <abdul.azeem@daimler.com>
2021-01-20 11:14:57 +0530

    added groovy file

---
+++ DSL.groovy
@@ -0,0 +1,15 @@
+job('Maven DSL project'){
+    description("first maven project for DSl script")
+    scm{
+        git("https://github.com/AR-Azeem/MavenPrac.git",master)
+    }
+    triggers{
+        scm('* * * * *')
+    }
+    steps{
+        maven('clean package','pom.xml')
+    }
+    publishers{
+        archiveArtifacts '**/*.jar'
+    }
+}
